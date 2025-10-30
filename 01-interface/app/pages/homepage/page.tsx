0c6f118dbefa358c17f5053d93f75c27c6551fed
K1enNg <hiimkien2702@gmail.com>
2025-10-30 12:01:00 -0400

    built homepage template

---
+++ 01-interface/app/pages/homepage/page.tsx
@@ -0,0 +1,12 @@
+import Headbar from '../../components/headbar'
+
+const Homepage = () => {
+    return (
+        <>
+            <Headbar />
+            <div className="flex justify-center items-center h-screen text-2xl font-bold">Welcome to Kiengineer</div>
+        </>
+    )
+}
+
+export default Homepage
