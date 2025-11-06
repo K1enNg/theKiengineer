4db2321d62d22aaac8a51cfd3b54e42f6314d224
Kinkin <hiimkien2702@gmail.com>
2025-11-05 21:04:56 -0500

    modified post editor

--- 01-interface/components/sidebar.tsx
+++ 01-interface/components/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Home, Inbox, Settings } from "lucide-react"
+import { Home, Settings, PencilIcon } from "lucide-react"
 
 import {
   Sidebar,
@@ -10,6 +10,9 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
 
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
 const author = {
   name: "John Doe",
   avatar: "https://imgs.search.brave.com/47pR8CI7w3lgajXcH8nWAYgg1mUoZKs7Qw7435zLpx8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9mYXN0/bHktczMuYWxsbXVz/aWMuY29tL2FydGlz/dC9tbjAwMDAxODM0/MzAvNDAwLzVwNUtk/OUstTTZnbjJvS1lp/QTNXVGo2S3NNdHRM/bHlCbW1WVFo2X0NM/czA9LmpwZw",
@@ -20,11 +23,6 @@ const items = [
     title: "Home",
     url: "/dashboard/postlist",
     icon: Home,
-  },
-  {
-    title: "Posts",
-    url: "/dashboard/compose",
-    icon: Inbox,
   },
   {
     title: "Settings",
@@ -45,16 +43,22 @@ export function AppSidebar() {
           />
           <span className="mt-2 text-sm font-medium">{author.name}</span>
         </div>
+        <Button asChild variant="outline" className="self-center flex items-center justify-center gap-2">
+          <Link href="/dashboard/compose">
+            Write
+            <PencilIcon />
+          </Link>
+        </Button>
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
-                    <a href={item.url}>
+                    <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
-                    </a>
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
