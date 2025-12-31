7ed683477085cb7d53c077c807b34bb6589f9a01
Kinkin <hiimkien2702@gmail.com>
2025-12-31 01:03:05 -0500

    configuration for deployment

--- 01-interface/middleware.ts
+++ 01-interface/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { ROUTES } from "@/config/routes";
+import { ROUTES } from "@/lib/routes";
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('access_token')?.value;
