eafec108d219d5920d90c2d0c858c60e9b397833
Kinkin <hiimkien2702@gmail.com>
2026-01-06 13:46:28 -0500

    deployment debug

--- 02-server/src/prisma/prisma.service.ts
+++ 02-server/src/prisma/prisma.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
